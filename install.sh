92c9fb796b5b2ff13510660ad65a2532960c0678
Vinicius Lee <vinilee34@gmail.com>
2024-07-21 00:27:55 -0300

    readme

---
+++ install.sh
@@ -0,0 +1,14 @@
+docker run --rm \
+    -u "$(id -u):$(id -g)" \
+    -v "$(pwd):/var/www/html" \
+    -w /var/www/html \
+    laravelsail/php82-composer:latest \
+    composer install --ignore-platform-reqs
+
+cp .env.example .env
+
+echo "subindo containers";
+vendor/bin/sail up -d
+
+vendor/bin/sail artisan key:generate
+vendor/bin/sail artisan migrate
